c2036d27d41fe7f1f3c5f338106d0bacece4f0a3
Horasan, Riza <rizahorasan@gmail.com>
2021-02-23 10:44:43 +0100

    new questions.

--- src/main/java/com/horasan/cs_dojo/samples/EasySamples.java
+++ src/main/java/com/horasan/cs_dojo/samples/EasySamples.java
@@ -1,7 +1,10 @@
 package com.horasan.cs_dojo.samples;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Hashtable;
+import java.util.List;
+import java.util.Stack;
 
 public class EasySamples {
 
@@ -130,9 +133,105 @@ public class EasySamples {
 			return sign * result;
 	}
 
+	/*
+	Given an integer x, return true if x is palindrome integer.
+	An integer is a palindrome when it reads the same backward as forward. 
+	For example, 121 is palindrome while 123 is not.
+		System.out.println(isPalindrome(-12821));
+	*/
+	public static boolean isPalindrome(int x) {
+		
+		List<Integer> numbers = new ArrayList<>();
+		int number = x;
+		int num = -1;
+		int reversed = 0;
+		while (number > 0) {
+			num = number % 10;
+			numbers.add(num);
+			number = number / 10;
+			reversed = reversed * 10 + num;
+		}
+		
+		if (x == reversed) {
+			return true;
+		}
+		else {
+			return false;
+		}
+		
+	
+	}
+	
+	/*
+		Write a function to find the longest common prefix string amongst an array of strings.
+		If there is no common prefix, return an empty string "".
+	
+		Input: strs = ["flower","flow","flight"]
+		Output: "fl"
+	
+
+		Input: strs = ["dog","racecar","car"]
+		Output: ""
+		Explanation: There is no common prefix among the input strings.
+	*/
+	
+	public static String longestCommonPrefix(String[] strs) {
+
+		if (strs == null) {
+			return "";
+		}
+		if(strs.length == 1) {
+			return strs[0]; 
+		}
+		
+		int minLen = Integer.MAX_VALUE;
+
+		int inputLen = strs.length;
+
+		for (int i = 0; i < inputLen; i++) {
+			minLen = Math.min(minLen, strs[i].length());
+		}
+
+		StringBuffer result = new StringBuffer("");
+		String aa = "";
+		boolean same = false;
+		for (int i = 0; i < minLen; i++) {
+			aa = "";
+			same = false;
+			for (int j = 0; j < inputLen; j++) {
+
+				if (aa == "") {
+					aa = String.valueOf(strs[j].charAt(i));
+					same = false;
+				} else {
+					if (aa.equals(String.valueOf(strs[j].charAt(i)))) {
+
+						same = true;
+					} else {
+						same = false;
+						break;
+					}
+				}
+			}
+
+			if (same) {
+				result.append(aa);
+			} else {
+				break;
+			}
+
+		}
+
+		return result.toString();
+	}
+				
+	
 	public static void main(String[] args) {
 		
-		System.out.println(myAtoi("-2147483647"));
+		
+		String [] bb = new String [] {"flower","flow","flight"};
+		String aa = longestCommonPrefix(bb);
+		
 	}
 	
 }
